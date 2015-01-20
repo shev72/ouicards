8492db5f078052c9303c456b04b9aa551438a885
Alou <als31@aber.ac.uk>
2015-01-20 12:40:41 +0000

    first

---
+++ live/settings.js
@@ -0,0 +1,12 @@
+var mySettings ={};
+
+mySettings['useProgressBar'] = true;
+mySettings['maxSecondsUntilWrong'] =5;
+mySettings['secsBeforeCorrectDisplayed'] =3;
+
+mySettings['firstSideTime'] =3;
+mySettings['secondSideTime'] =3;
+mySettings['nbCardsInGroup'] =3;
+mySettings['nbGroupRepetition'] =3;
+
+mySettings['mode'] ='b';
